f3a753a209116ef3faf8888a8f42b3a3c8d5c3c5
Frederik Wagner <wagner@wagit.de>
2014-07-03 11:27:55 +0200

    export sshkeys only on set storeconfigs

--- manifests/server/sshkeys.pp
+++ manifests/server/sshkeys.pp
@@ -1,27 +1,32 @@
 # make ssh host keys availabe (private)
 class ssh::server::sshkeys {
 
-  # collect all ipadresses as alias for the host keys
-  $ipaddresses = ipaddresses()
-  $host_aliases = flatten([ $::fqdn, $::hostname, $ipaddresses ])
+  # only when storeconfigs ist eneabled!
+  if $::storeconfigs {
 
-  @@sshkey { "${::fqdn}_dsa":
-    host_aliases => $host_aliases,
-    type         => dsa,
-    key          => $::sshdsakey,
-  }
-  @@sshkey { "${::fqdn}_rsa":
-    host_aliases => $host_aliases,
-    type         => rsa,
-    key          => $::sshrsakey,
-  }
-  if $::sshecdsakey {
-    @@sshkey { "${::fqdn}_ecdsa":
+    # collect all ipadresses as alias for the host keys
+    $ipaddresses = ipaddresses()
+    $host_aliases = flatten([ $::fqdn, $::hostname, $ipaddresses ])
+
+    @@sshkey { "${::fqdn}_dsa":
       host_aliases => $host_aliases,
-      type         => 'ecdsa-sha2-nistp256',
-      key          => $::sshecdsakey,
+      type         => dsa,
+      key          => $::sshdsakey,
     }
-  }
+    @@sshkey { "${::fqdn}_rsa":
+      host_aliases => $host_aliases,
+      type         => rsa,
+      key          => $::sshrsakey,
+    }
+    if $::sshecdsakey {
+      @@sshkey { "${::fqdn}_ecdsa":
+        host_aliases => $host_aliases,
+        type         => 'ecdsa-sha2-nistp256',
+        key          => $::sshecdsakey,
+      }
+    }
 
+
+  }
 
 }
