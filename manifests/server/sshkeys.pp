5366d87d65885963e52cd44e7d871541d72e3199
Frederik Wagner <wagner@wagit.de>
2015-11-24 21:29:18 +0100

    export potential ssh ed25519 hostkey, remove ssh 1 and dsa host keys

--- manifests/server/sshkeys.pp
+++ manifests/server/sshkeys.pp
@@ -5,13 +5,11 @@ class ssh::server::sshkeys {
   $ipaddresses = ipaddresses()
   $host_aliases = flatten([ $::fqdn, $::hostname, $ipaddresses, $ssh::server::host_aliases ])
 
-  if $::sshdsakey {
-    @@sshkey { "${::fqdn}_dsa":
-      host_aliases => $host_aliases,
-      type         => dsa,
-      key          => $::sshdsakey,
-    }
+  # ensure insecure hostfiles removed
+  file { ['/etc/ssh/ssh_host_key', '/etc/ssh/ssh_host_dsa_key']:
+    ensure => absent,
   }
+
   if $::sshrsakey {
     @@sshkey { "${::fqdn}_rsa":
       host_aliases => $host_aliases,
@@ -24,6 +22,14 @@ class ssh::server::sshkeys {
       host_aliases => $host_aliases,
       type         => 'ecdsa-sha2-nistp256',
       key          => $::sshecdsakey,
+    }
+  }
+
+  if $::sshed25519key {
+    @@sshkey { "${::fqdn}_ed25519":
+      host_aliases => $host_aliases,
+      type         => 'ssh-ed25519',
+      key          => $::sshed25519key,
     }
   }
 
